dee224dd1b8c23e964ecd150cc880c4a4d5c2c29
KishanAkbari05 <160114350+KishanAkbari05@users.noreply.github.com>
2024-03-09 12:54:20 +0530

    Add files via upload

---
+++ Commandinput.java
@@ -0,0 +1,12 @@
+public class Commandinput{
+	public static void main(String[] args){
+
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+
+
