2d46a5aa14363b49414655f1bd86edd1a9a64f0b
KishanAkbari05 <160114350+KishanAkbari05@users.noreply.github.com>
2024-03-15 15:57:58 +0530

    Add files via upload

---
+++ CommandSum1.java
@@ -0,0 +1,47 @@
+public class CommandSum1{
+	public static void main(String[] args){
+
+		double a = Double.parseDouble(args[0]);
+		double b = Double.parseDouble(args[1]);
+
+		System.out.println("Sum = " + (a+b));
+	}
+}
+
+// class Sum{
+// 	int a;
+// 	int b;
+
+// 	public int add(){
+// 		return a+b;
+// 	}
+// }
+// 	public class CommandSum1{
+// 		public static void main(String[] args) {
+// 			Sum s1 = new Sum();
+// 			s1.a=5;
+// 			s1.b=6;
+// 			int c=s1.add();
+// 			System.out.println(c);	
+// 		}
+// 	}
+
+// public class CommandSum1{
+//  	public static void main(String[] args){
+//  		int i=5;
+//  		int j=2;
+//  		int k=15;
+//  		System.out.println(i);
+ 		
+//  		// int k=i--%(i-1);
+//  		// System.out.println(k);
+//  		// System.out.println(i);
+
+//  		i-=--j + --j;
+//  		System.out.println(i);
+//  		System.out.println(j);
+
+
+// 	}
+// }
+
